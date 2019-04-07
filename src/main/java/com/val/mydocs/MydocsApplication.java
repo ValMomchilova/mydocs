92a6a10d50ff41393556072b5e50521be0eee3c5
Val <val12mo@gmail.com>
2019-04-07 17:33:31 +0300

    Initial upload

---
+++ src/main/java/com/val/mydocs/MydocsApplication.java
@@ -0,0 +1,13 @@
+package com.val.mydocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MydocsApplication.class, args);
+    }
+
+}
